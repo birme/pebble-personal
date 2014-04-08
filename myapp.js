88576d013a487579939cd2bf9bbdbda20de8cad8
Jonas Birme <jonas.birme@gmail.com>
2014-04-08 09:00:45 +0200

    Need button click first

--- myapp.js
+++ myapp.js
@@ -1,13 +1,14 @@
-simply.scrollable(true);
+simply.scrollable(false);
 simply.title('Welcome!');
 simply.body('I am your personal Pebble assistant', true);
-navigator.geolocation.getCurrentPosition(function(pos) {
-  var coords = pos.coords;
-  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
-      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
-  ajax({ url: weatherUrl, type: 'json' }, function(data) {
-  	simply.title('Jonas, ');
-  	simply.subtitle(data.name);
-    simply.body('Temperature at your location is ' + data.main.temp, false);
-  });
+simply.on('singleClick', function(e) {
+
+	navigator.geolocation.getCurrentPosition(function(pos) {
+  		var coords = pos.coords;
+  		var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      		'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  		ajax({ url: weatherUrl, type: 'json' }, function(data) {
+  			simply.text( {title: 'Jonas, ', subtitle: data.name, body: 'Temperature at your location is ' + data.main.temp + ' degrees (C)'} );
+  		});
+	});
 });

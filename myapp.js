ed4801473a4a994b116cbfdc95e25d2fe736bb59
Jonas Birme <jonas.birme@gmail.com>
2014-04-08 08:51:58 +0200

    Some small text tweaks

--- myapp.js
+++ myapp.js
@@ -1,11 +1,13 @@
 simply.scrollable(true);
-simply.title('Welcome Jonas!');
+simply.title('Welcome!');
 simply.body('I am your personal Pebble assistant', true);
 navigator.geolocation.getCurrentPosition(function(pos) {
   var coords = pos.coords;
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.body('Temperature at your location is ' + data.main.temp);
+  	simply.title('Jonas, ');
+  	simply.subtitle(data.name);
+    simply.body('Temperature at your location is ' + data.main.temp, false);
   });
 });

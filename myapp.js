93f74a9947e599103df3e3133a9597056c90fc22
Jonas Birme <jonas.birme@gmail.com>
2014-04-08 08:13:27 +0200

    Show the weather

--- myapp.js
+++ myapp.js
@@ -1,2 +1,11 @@
+simply.scrollable(true);
 simply.title('Welcome Jonas!');
-simply.body('I am your personal Pebble assistant');
+simply.body('I am your personal Pebble assistant', true);
+navigator.geolocation.getCurrentPosition(function(pos) {
+  var coords = pos.coords;
+  var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
+      'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
+  ajax({ url: weatherUrl, type: 'json' }, function(data) {
+    simply.text({ title: data.name, subtitle: data.main.temp });
+  });
+});

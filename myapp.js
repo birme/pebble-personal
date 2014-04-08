3d2555f26c53ec7d9a195ea6d9134a142b9f51a3
Jonas Birme <jonas.birme@gmail.com>
2014-04-08 08:15:45 +0200

    Show the temp

--- myapp.js
+++ myapp.js
@@ -6,6 +6,6 @@ navigator.geolocation.getCurrentPosition(function(pos) {
   var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?' +
       'lat=' + coords.latitude + '&lon=' + coords.longitude + '&units=metric';
   ajax({ url: weatherUrl, type: 'json' }, function(data) {
-    simply.text({ title: data.name, subtitle: data.main.temp });
+    simply.body('Temperature at your location is ' + data.main.temp);
   });
 });
